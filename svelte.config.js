d88fdf70ee3b442d964d9c8c55d822e66926f41f
Imanuel Ulbricht <me@imanuel.dev>
2023-02-12 04:44:20 +0100

    Update to use node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      out: 'build'
+    })
+  }
 };
 
 export default config;
