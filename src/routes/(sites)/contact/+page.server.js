8f91fd558de67a8f7f7f509a21c70b2383c5dfc5
Imanuel Ulbricht <me@imanuel.dev>
2023-07-27 01:33:08 +0200

    Fix code style isseus

--- src/routes/(sites)/contact/+page.server.js
+++ src/routes/(sites)/contact/+page.server.js
@@ -1,38 +1,38 @@
-import * as nodemailer from 'nodemailer';
+import * as nodemailer from "nodemailer";
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-  default: async ({ request }) => {
-    try {
-      const data = await request.formData();
-      const email = data.get('email');
-      const name = data.get('name');
-      const concern = data.get('concern');
-      const message = data.get('message');
+	default: async ({ request }) => {
+		try {
+			const data = await request.formData();
+			const email = data.get('email');
+			const name = data.get('name');
+			const concern = data.get('concern');
+			const message = data.get('message');
 
-      const transport = nodemailer.createTransport({
-        host: process.env.MAIL_SERVER,
-        port: process.env.MAIL_PORT,
-        secure: false,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS
-        }
-      });
-      const mail = {
-        from: 'noreply@jinya.de',
-        to: 'developers@jinya.de',
-        subject: concern,
-        text: `From: ${name}
+			const transport = nodemailer.createTransport({
+				host: process.env.MAIL_SERVER,
+				port: process.env.MAIL_PORT,
+				secure: false,
+				auth: {
+					user: process.env.MAIL_USER,
+					pass: process.env.MAIL_PASS
+				}
+			});
+			const mail = {
+				from: 'noreply@jinya.de',
+				to: 'developers@jinya.de',
+				subject: concern,
+				text: `From: ${name}
 Email: ${email}
 Message:
 ${message}`
-      };
-      await transport.sendMail(mail);
+			};
+			await transport.sendMail(mail);
 
-      return { success: true };
-    } catch (e) {
-      return { success: false };
-    }
-  }
+			return { success: true };
+		} catch (e) {
+			return { success: false };
+		}
+	}
 };
