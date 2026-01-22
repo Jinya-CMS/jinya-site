91c38481693267d5cb8edf29c0c99bfa412f2266
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2026-01-22 09:38:28 +0100

    Chore: Bump dependencies

--- src/routes/(sites)/about/+page.svelte
+++ src/routes/(sites)/about/+page.svelte
@@ -16,9 +16,9 @@
 		We achieved that by providing two different ways of managing text and artworks. Texts work with the standard editor you know from other
 		CMS. The artwork part on the other hand is based on a designer where you can order your artworks in the order you want. This order is
 		then reflected in the page your users and fans see.<br />
-		In September 2021 Christina Ruebsam, wanted to start an art and crafts blog. She liked the idea behind Jinya. It was lacking support for
-		blogging though. So we integrated a blogging system. As of today Jinya contains a way to arrange artworks, create simple text pages, forms
-		and the new blogging system.
+		In September 2021 Christina Ruebsam, wanted to start an art and crafts blog. She liked the idea behind Jinya. It was lacking support for blogging
+		though. So we integrated a blogging system. As of today Jinya contains a way to arrange artworks, create simple text pages, forms and the
+		new blogging system.
 	</p>
 </Article>
 
